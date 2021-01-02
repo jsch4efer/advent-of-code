ae7678e99f8cf42f0558352c089e847e111cd9d3
Julian Schäfer <schaefer.julian@mail.de>
2021-01-02 23:55:14 +0100

    Add solution to day 6

---
+++ src/advent_of_code_2020/day_6.clj
@@ -0,0 +1,71 @@
+(ns advent-of-code-2020.day-6
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]))
+
+(defn parse-answers [input]
+  (->> input
+       string/split-lines
+       (partition-by #(when (= % "") :split))
+       (filter #(not= % '("")))))
+
+(defn count-yes-for-anyone [answers]
+  (->> answers
+       (map #(->> % (mapcat seq) (into #{})))
+       (map count)
+       (reduce +)))
+
+(defn count-yes-for-everyone [answers]
+  (->> answers
+       (map (fn [group]
+              (->> group
+                   (map #(->> % seq (into #{})))
+                   (reduce clojure.set/intersection))))
+       (map count)
+       (reduce +)))
+
+(comment
+
+  ;; Part 1
+
+  (def example-data "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b")
+
+  (->> example-data
+       parse-answers
+       count-yes-for-anyone)
+  ;; => 11
+
+
+  (->> (slurp (io/resource "day-6"))
+       parse-answers
+       count-yes-for-anyone)
+  ;; => 6878
+
+
+
+
+  ;; Part 2
+
+  (->> example-data
+       parse-answers
+       count-yes-for-everyone)
+  ;; => 6
+
+  (->> (slurp (io/resource "day-6"))
+       parse-answers
+       count-yes-for-everyone)
+
+  )
