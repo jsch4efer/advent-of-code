3e1fb106df44f87ef68e864319f06aaf37a3452f
Julian Schäfer <schaefer.julian@mail.de>
2021-12-03 01:36:16 +0100

    Add solution for day 11 for year 2020

--- src/advent_of_code/2020/day_11.clj
+++ src/advent_of_code/2020/day_11.clj
@@ -2,7 +2,46 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as string]))
 
-(def example "L.LL.LL.LL
+
+(defn parse-seats [input]
+  (->> input
+       string/split-lines
+       (map (fn [line]
+              (->> line
+                   (map #(case % \L 0 \. nil \# 1) )
+                   (into []))))
+       (into [])))
+
+(defn count-occupied-seats [seats]
+  (->> seats
+       (mapcat identity)
+       (filter identity)
+       (reduce +))
+  )
+
+(defn seat-round [seats count-occupied-neighbours seat-rules]
+  (->>
+   (for [x (-> seats first count range)
+         y (-> seats count range)]
+     [[y x] (count-occupied-neighbours seats [y x])])
+   (reduce (fn [last [pos n]]
+             (assoc-in last pos (seat-rules (get-in seats pos) n)))
+            seats)))
+
+(defn simulate-seats [input count-occupied-neighbours seat-rules]
+  (loop [seats (parse-seats input)]
+    (let [next-seats (seat-round seats count-occupied-neighbours seat-rules)]
+      (if (= seats next-seats)
+        next-seats
+        (recur next-seats))
+      )
+    ))
+
+(comment
+
+  ;; Part 1
+
+  (def example "L.LL.LL.LL
 LLLLLLL.LL
 L.L.L..L..
 LLLL.LL.LL
@@ -13,85 +52,93 @@ LLLLLLLLLL
 L.LLLLLL.L
 L.LLLLL.LL")
 
-(defn- extend-waiting-room [waiting-room]
-  (let [horizontal-extension (into [] (repeat (count (first waiting-room)) 0))]
-    (->> (concat [horizontal-extension] waiting-room [horizontal-extension])
-         (map #(into [] (concat [0] % [0])) )
-         (into []))))
+  (def seat-range-part-1 (fn [seats [y x]]
+                           (->> (for [y-n (range (dec y) (+ y 2))
+                                      x-n (range (dec x) (+ x 2))]
+                                  [y-n x-n])
+                                (filter (partial not= [y x]))
+                                (map (partial get-in seats))
+                                (filter identity)
+                                (reduce +))))
 
-(defn- occupied-seats-near-by [waiting-room [pos-x pos-y]]
-  (let [correction-term (case (get-in waiting-room [pos-y pos-x])
-                          1 1
-                          0)]
-    (->> (for [x (range (dec pos-x) (+ pos-x 2))
-               y (range (dec pos-y) (+ pos-y 2))]
-           (get-in waiting-room [y x]))
-         (filter (partial = 1))
-         (reduce (fn [res _] (inc res)))
-         (apply #(- % correction-term))
-         )))
+  (def seat-rule-part-1 (fn [seat neighbor-count]
+                          (case seat
+                            0 (if (= neighbor-count 0) 1 0)
+                            1 (if (<= 4 neighbor-count) 0 1)
+                            nil nil
+                            )))
 
-(defn- simulate-waiting-room-step [waiting-room]
-  (let [extended-waiting-room (extend-waiting-room waiting-room)]
-    (->> waiting-room
-         (map-indexed
-          (fn [y row]
-            (->> (map-indexed (fn [x seat]
-                                    (let [occupied (occupied-seats-near-by extended-waiting-room [(inc x) (inc y)])]
-                                      (cond
-                                        (and (= seat 0) (= occupied 0)) 1
-                                        (and (= seat 1) (<= 4 occupied)) 0
-                                        :else seat)))
-                              row)
-                 (into []))))
-         (into []))))
+  (count-occupied-seats (simulate-seats example seat-range-part-1 seat-rule-part-1))
+  ;; => 37
 
-(defn- count-occupied-seats [waiting-room]
-  (->> waiting-room
-       (mapcat identity)
-       (reduce (fn [res next] (if (= next 1) (inc res) res)) 0))
+  (def input (-> "2020/day-11" io/resource slurp))
+
+  (count-occupied-seats (simulate-seats input seat-range-part-1 seat-rule-part-1))
+  ;; => 2204
+
+  ;; Part 2
+
+  (def example-2 ".......#.
+...#.....
+.#.......
+.........
+..#L....#
+....#....
+.........
+#........
+...#.....")
+
+  (def seat-range-part-2 (fn [seats [y x]]
+                           (let [max-x-pos (-> seats first count dec)
+                                 max-y-pos (-> seats count dec)
+                                 valid-pos? (fn [[y x]]
+                                              (and (<= 0 y max-y-pos)
+                                                   (<= 0 x max-x-pos)))
+                                 gen-pos (fn [pos steps]
+                                           (->> (iterate
+                                                    (fn [last-pos] [(+ (first last-pos) (first steps))
+                                                                    (+ (second last-pos) (second steps))])
+                                                    pos)
+                                                (drop 1)
+                                                (take-while valid-pos?)
+                                                ))
+                                 occupied-neighbor (fn [seats direction]
+                                                     (or (->> direction
+                                                              (map (partial get-in seats))
+                                                              (filter #(or (= 1 %) (= 0 %)))
+                                                              (first))
+                                                         0))
+                                 ]
+                           (->> [[0 1] [0 -1] [1 0] [-1 0] [1 1] [1 -1] [-1 1] [-1 -1]]
+                                (map #(gen-pos [y x] %))
+                                (map #(occupied-neighbor seats %))
+                                (reduce +)
+                                )
+
+                           )))
+
+  (def seat-rule-part-2 (fn [seat neighbor-count]
+                          (case seat
+                            0 (if (= neighbor-count 0) 1 0)
+                            1 (if (<= 5 neighbor-count) 0 1)
+                            nil nil
+                            )))
+
+ (def example-empty ".##.##.
+#.#.#.#
+##...##
+...L...
+##...##
+#.#.#.#
+.##.##.")
+
+  (count-occupied-seats (simulate-seats example-2 seat-range-part-2 seat-rule-part-2))
+;; => 8
+
+  (count-occupied-seats (simulate-seats example seat-range-part-2 seat-rule-part-2))
+;; => 26
+
+  (count-occupied-seats (simulate-seats input seat-range-part-2 seat-rule-part-2))
+;; => 1986
+
   )
-
-(defn- print-and-pass [round x]
-  (println "Round" round (count-occupied-seats x))
-    x)
-
-
-(comment
-  (let [parsed (->> example
-                    string/split-lines
-                    (map (fn [line]
-                           (->> line
-                                (map #(case % \L 0 \. nil \# 1) )
-                                (into []))))
-                    (into []))]
-    (->> parsed
-         simulate-waiting-room-step
-         ;; (print-and-pass 1)
-         ;; simulate-waiting-room-step
-         ;; (print-and-pass 2)
-         ;; simulate-waiting-room-step
-         ;; (print-and-pass 3)
-         ;; simulate-waiting-room-step
-         ;; (print-and-pass 4)
-         ;; simulate-waiting-room-step
-         ;; (print-and-pass 5)
-         ;; simulate-waiting-room-step
-         ;; (print-and-pass 6)
-         ;; simulate-waiting-room-step
-         ;; (print-and-pass 7)
-         ;; simulate-waiting-room-step
-         ;; (print-and-pass 8)
-
-         ))
-
-
-  )
-
-#_(loop [current-waiting-room parsed
-            steps 0]
-       (let [next-waiting-room (simulate-waiting-room-step current-waiting-room)]
-         (if (= current-waiting-room next-waiting-room)
-           steps
-           (recur next-waiting-room (inc steps)))
-         ))
