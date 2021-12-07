c16fa5fc2cbfb2516ec453e2d1121f6c2a041f85
Julian Schäfer <schaefer.julian@mail.de>
2021-12-07 06:31:08 +0100

    Add solution part 2 for day 7 2021 and refactor code

--- src/advent_of_code/2021/day_7.clj
+++ src/advent_of_code/2021/day_7.clj
@@ -3,33 +3,41 @@
             [clojure.set :as set]
             [clojure.java.io :as io]))
 
+(defn parse-crab-positions [input]
+  (->>  (string/split input #",") (map read-string)))
+
+(defn find-minimum-steps [crabs cost-fn]
+    (apply min (for [p (range 0 (apply max crabs))]
+                 (->> crabs
+                      (map (partial cost-fn p ))
+                      (reduce +))
+                 ))
+      )
 
 (comment
   ;; Part 1
 
-  (let [crabs (->>
-               #_(string/split  "16,1,2,0,4,2,7,1,2,14" #",")
-               (string/split (slurp (io/resource "2021/day-7")) #"," )
+  (def example-input   "16,1,2,0,4,2,7,1,2,14")
 
-               (map read-string)
-               )]
-    (apply min (for [p (range 0 (apply max crabs))]
-                 (->> crabs
-                      (map (fn [crab] (Math/abs (- crab p))))
-                      (reduce +))
-                 ))
-    )
+  (def input (slurp (io/resource "2021/day-7")))
 
-  (->> (slurp (io/resource "2021/day-7"))
-       )
+  (def cost-fn-part1 (fn [pos crab] (Math/abs (- pos crab))))
 
+  (find-minimum-steps (parse-crab-positions example-input) cost-fn-part1)
+;; => 37
+
+  (find-minimum-steps (parse-crab-positions input) cost-fn-part1)
+;; => 331067
 
   ;; Part 2
 
+  (def cost-fn-part2 (fn [pos crab] (let [n (Math/abs (- crab pos))]
+                                        (/ (* (+ n 1) n) 2)) ))
 
-  (->> (slurp (io/resource "2021/day-6")) parse-fish (count-fish 256))
+  (find-minimum-steps (parse-crab-positions example-input) cost-fn-part2)
+;; => 168
 
-
-
+  (find-minimum-steps (parse-crab-positions input) cost-fn-part2)
+;; => 92881128
 
 )
