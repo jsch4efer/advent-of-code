40c3a78ea937dd8e52f2c7c9c5369600cd627125
Julian Schäfer <schaefer.julian@mail.de>
2021-12-10 00:07:54 +0100

    refactor: reduce duplicate logic in day 9

--- src/advent_of_code/2021/day_9.clj
+++ src/advent_of_code/2021/day_9.clj
@@ -3,7 +3,6 @@
             [clojure.set :as set]
             [clojure.java.io :as io]))
 
-
 (defn parse-height-map [input]
   (->> (string/split-lines input)
        (map #(->> (seq %)
@@ -12,36 +11,11 @@
                   (into [])))
        (into [])))
 
-(defn solve-part1 [input]
-  (let [height-map (parse-height-map input)]
-    (->>
-     (for [x (range 0 (-> height-map first count))
-           y (range 0 (-> height-map count))]
-       (let [h (get-in height-map [y x])]
-         (when (->> [[(dec y) x]
-                     [y (inc x)]
-                     [(inc y) x]
-                     [y (dec x)]]
-                    (map #(get-in height-map % Integer/MAX_VALUE))
-                    (map (partial < h))
-                    (reduce (fn [res next] (and res next)) true)
-                    )
-           h)
-         )
-       )
-     (filter identity)
-     (map inc)
-     (reduce +)
-     )
-    ))
-
-
 (defn point->neighbors [x y]
   [[y (inc x)]
    [(inc y) x]
    [y (dec x)]
-   [(dec y) x]
-   ])
+   [(dec y) x]])
 
 (defn to-direction [[y1 x1] [y2 x2]]
   (let [dx (- x1 x2)
@@ -51,22 +25,25 @@
       (and (= 0 dx) (< 0 dy)) :U
       (and (< 0 dx) (= 0 dy)) :L
       (and (> 0 dx) (= 0 dy)) :R
-      :else :S
-      )
-    )
-  )
+      :else :S)))
 
 (defn find-low-points [height-map]
-                   (->> (for [x (range 0 (-> height-map first count))
-                              y (range 0 (-> height-map count))]
-                          (let [h (get-in height-map [y x])]
-                            (when (->> (point->neighbors x y)
-                                       (map #(get-in height-map % Integer/MAX_VALUE))
-                                       (map (partial < h))
-                                       (reduce (fn [res next] (and res next)) true)
-                                       )
-                              [y x])))
-                        (filter identity)))
+  (->> (for [x (range 0 (-> height-map first count))
+             y (range 0 (-> height-map count))]
+         (let [h (get-in height-map [y x])]
+           (when (->> (point->neighbors x y)
+                      (map #(get-in height-map % Integer/MAX_VALUE))
+                      (map (partial < h))
+                      (reduce (fn [res next] (and res next)) true))
+             [y x])))
+       (filter identity)))
+
+(defn solve-part1 [input]
+  (let [height-map (parse-height-map input)]
+    (->> (find-low-points height-map)
+         (map (partial get-in height-map))
+         (map inc)
+         (reduce +))))
 
 (defn calculate-directions [height-map width height]
   (->> (for [x (range 0 width)
@@ -81,10 +58,8 @@
                                      [p vmin]
                                      [n v]))
                                  [p 0])
-                         first)
-               ]
-           [p nmin])
-         )
+                         first)]
+           [p nmin]))
        (map (fn [[p n]] [p (to-direction p n)]))
        (reduce (fn [res [p n]] (assoc-in res p n))
                height-map)))
@@ -97,44 +72,38 @@
                  (update-in res next (fn [height] [height (get-in directions next)])))
                heights)))
 
-
 (defn collect-basins [heights-and-directions width height]
   (let [step (fn [[y x]]
                (let [[_ d] (get-in heights-and-directions [y x])
                      [ny nx] (case d
-                              :L [y (dec x)]
-                              :R [y (inc x)]
-                              :D [(inc y) x]
-                              :U [(dec y) x]
-                              :S [y x])]
+                               :L [y (dec x)]
+                               :R [y (inc x)]
+                               :D [(inc y) x]
+                               :U [(dec y) x]
+                               :S [y x])]
                  (if (and (<= 0 nx (dec width)) (<= 0 ny (dec height)))
                    [ny nx]
-                   [y x])
-                 ))]
+                   [y x])))]
     (->> (for [x (range 0 width)
                y (range 0 height)]
            [y x])
          (filter (fn [p] (not= 9 (first (get-in heights-and-directions p)))))
          (reduce (fn [res p]
                    (let [path (->> (iterate step p)
-                                    (partition 2 1)
-                                    (take-while (partial apply not=))
-                                    (reduce concat))
+                                   (partition 2 1)
+                                   (take-while (partial apply not=))
+                                   (reduce concat))
                          last (last path)]
                      (update res last
                              (fn [basin]
                                (set/union (or basin #{}) (set path))))))
-                  {})
-         (filter first)
-        ))
-  )
-
+                 {})
+         (filter first))))
 
 (defn get-largest-n-basins [basins n]
   (->> basins
        (sort-by #(-> % second count))
        (take-last n)))
-
 
 (defn solve-part2 [input]
   (let [heights (parse-height-map input)
@@ -142,13 +111,10 @@
         height (-> heights count)
         directions  (calculate-directions heights width height)
         heights-and-directions (combine-heights-and-directions heights directions width height)
-        basins (collect-basins heights-and-directions width height)
-        ]
+        basins (collect-basins heights-and-directions width height)]
     (->> (get-largest-n-basins basins 3)
          (map (comp count second))
-         (reduce *))
-    ))
-
+         (reduce *))))
 
 (comment
 
@@ -177,6 +143,4 @@
 
   (solve-part2 (slurp (io/resource "2021/day-9")))
 ;; => 1113424
-
-
   )
