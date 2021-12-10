311ae7c27faf1daa359009b0e558839624cfe5b5
Julian Schäfer <schaefer.julian@mail.de>
2021-12-10 23:53:07 +0100

    Chore: Apply code formatting rules for day 2 2020

--- src/advent_of_code/2021/day_2.clj
+++ src/advent_of_code/2021/day_2.clj
@@ -9,8 +9,7 @@
        (map (fn [[cmd param]] [(keyword cmd) (read-string param)]))))
 
 (defn run-submarine [init-state interpretation commands]
-  (let [
-        final-state (->> commands
+  (let [final-state (->> commands
                          (reduce (fn [state cmd]
                                    ((interpretation cmd) state))
                                  init-state))]
@@ -22,10 +21,10 @@
 
 
   (defn part-1-interpreation [[action val]]
-         (case action
-           :forward (fn [s] (update s :horizontal + val))
-           :up  (fn [s] (update s :depth  + (* -1 val)))
-           :down (fn [s] (update s :depth + val))))
+    (case action
+      :forward (fn [s] (update s :horizontal + val))
+      :up  (fn [s] (update s :depth  + (* -1 val)))
+      :down (fn [s] (update s :depth + val))))
 
   (->> "forward 5
 down 5
@@ -34,32 +33,31 @@ up 3
 down 8
 forward 2
 "
-      (parse-submarine-commands)
-      (run-submarine
-       {:depth 0 :horizontal 0}
-       part-1-interpreation
-       ))
+       (parse-submarine-commands)
+       (run-submarine
+        {:depth 0 :horizontal 0}
+        part-1-interpreation))
 
 ;; => 150
+
 
   (->> (input/read-input! "2021/day-2")
        (parse-submarine-commands)
        (run-submarine
         {:depth 0 :horizontal 0 :aim 0}
-        part-1-interpreation
-        ))
+        part-1-interpreation))
 ;; => 2147104
 
 
   ;; Part 2
 
   (defn part-2-interpretation  [[cmd val]]
-          (case cmd
-            :forward (fn [s] (-> s
-                                 (update :horizontal + val)
-                                 (update :depth + (* (:aim s) val))))
-            :up (fn [s] (update s :aim + (* -1 val)))
-            :down (fn [s] (update s :aim + val))))
+    (case cmd
+      :forward (fn [s] (-> s
+                           (update :horizontal + val)
+                           (update :depth + (* (:aim s) val))))
+      :up (fn [s] (update s :aim + (* -1 val)))
+      :down (fn [s] (update s :aim + val))))
 
   (->> "forward 5
 down 5
@@ -71,16 +69,13 @@ forward 2
        (parse-submarine-commands)
        (run-submarine
         {:depth 0 :horizontal 0 :aim 0}
-        part-2-interpretation
-        ))
+        part-2-interpretation))
 ;; => 900
 
   (->> (input/read-input! "2021/day-2")
        (parse-submarine-commands)
        (run-submarine
         {:depth 0 :horizontal 0 :aim 0}
-        part-2-interpretation
-        ))
+        part-2-interpretation))
 ;; => 2044620088
-
   )
